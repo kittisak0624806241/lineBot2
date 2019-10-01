b8ccb6771f8a6200a8ef10c09de4b0495ca5cfb4
kittisak0624806241 <51777688+kittisak0624806241@users.noreply.github.com>
2019-10-01 14:10:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,8 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    #user = decoded["events"][0]['replyToken']
     user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
-    #userText = decoded["events"][0]['message']['text']
+    userText = decoded['queryResult']['intent']['displayName']
     #sendText(user,userText)
     if (userText == 'กินข้าวยัง') :
         sendText(user,'กินแล้ว')
@@ -32,7 +31,6 @@ def callback():
         sendText(user,'ทักมามีไรวะ')
         
     return '',200
-
 def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
   headers = {
